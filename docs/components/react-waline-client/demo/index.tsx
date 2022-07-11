140e453feb7b60febd73d133393f9d51a0beb230
eric_wang <jadeface.scholar@gmail.com>
2022-07-12 00:21:19 +0800

    feat: add nx and auto deploy

--- docs/components/react-waline-client/demo/index.tsx
+++ docs/components/react-waline-client/demo/index.tsx
@@ -1,23 +1,10 @@
-import Waline, { CRefProps } from "@montagejs/react-waline-client";
-import { FC, useRef } from "react";
-import { Button } from "antd";
+import Waline, { CRefProps } from '@montagejs/react-waline-client';
+import { FC, useRef } from 'react';
 
 const WalineDemo: FC = () => {
-
     const ref = useRef<CRefProps>(null);
 
-    const handleChange = () => {
-        ref.current?.update({
-            comment: true
-        })
-    }
-
-    return (
-        <>
-            <Waline ref={ref} serverURL="https://waline.bigdreamer.cc" />
-            <Button onClick={handleChange}>更新配置</Button>
-        </>
-    )
+    return <Waline ref={ref} serverURL="https://waline.bigdreamer.cc" />;
 };
 
 export default WalineDemo;
